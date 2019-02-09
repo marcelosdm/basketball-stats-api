31ac66717500922c6ed8d5302405ed34eb35c2c1
Marcelo Melo <marcelosdm@gmail.com>
2019-02-09 15:36:47 -0200

    Fix .env and mongo.connect

--- server.js
+++ server.js
@@ -9,18 +9,13 @@ app.use(express.json());
 app.use(cors());
 
 // Initialize DB
-// mongoose.connect('mongodb://localhost:27017/basketballdb', {
-//   useNewUrlParser: true
-// });
-// requireDir('./src/models');
 
-const url = process.env.MONGODB_URI;
+mongoose.connect(process.env.DATABASE_URL);
 mongoose.Promise = global.Promise;
-mongoose.connect(url);
 
 requireDir('./src/models');
 
 // Routes
 app.use('/api', require('./src/routes'));
 
-app.listen(process.env.PORT || port);
+app.listen(process.env.PORT || 3000);
